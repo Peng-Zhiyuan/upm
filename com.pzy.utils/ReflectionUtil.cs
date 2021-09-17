a2850079c354047bb07573286d03829ae29499ad
Tianyou <anso_y@163.com>
2021-09-17 14:09:21 +0800

    reflection util update

--- com.pzy.utils/ReflectionUtil.cs
+++ com.pzy.utils/ReflectionUtil.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System.Reflection;
 using System;
 using System.Linq;
+using System.Linq.Expressions;
 
 public static class ReflectionUtil
 {
@@ -68,6 +69,54 @@ public static class ReflectionUtil
             ret.AddRange(list);
         }
         return ret;
+    }
+    
+    public static Func<object, object[], object> GetExecuteDelegate(MethodInfo methodInfo)
+    {
+        // parameters to execute
+        ParameterExpression instanceParameter = Expression.Parameter(typeof(object), "instance");
+        ParameterExpression parametersParameter = Expression.Parameter(typeof(object[]), "parameters");
+
+        // build parameter list
+        List<Expression> parameterExpressions = new List<Expression>();
+        ParameterInfo[] paramInfos = methodInfo.GetParameters();
+        for (int i = 0; i < paramInfos.Length; i++)
+        {
+            // (Ti)parameters[i]
+            BinaryExpression valueObj = Expression.ArrayIndex(parametersParameter, Expression.Constant(i));
+            UnaryExpression valueCast = Expression.Convert(valueObj, paramInfos[i].ParameterType);
+
+            parameterExpressions.Add(valueCast);
+        }
+
+        // non-instance for static method, or ((TInstance)instance)
+        Expression instanceCast = methodInfo.IsStatic ? null :
+            Expression.Convert(instanceParameter, methodInfo.ReflectedType);
+
+        // static invoke or ((TInstance)instance).Method
+        MethodCallExpression methodCall = Expression.Call(instanceCast, methodInfo, parameterExpressions);
+        
+        // ((TInstance)instance).Method((T0)parameters[0], (T1)parameters[1], ...)
+        if (methodCall.Type == typeof(void))
+        {
+            Expression<Action<object, object[]>> lambda = Expression.Lambda<Action<object, object[]>>(
+                methodCall, instanceParameter, parametersParameter);
+
+            Action<object, object[]> execute = lambda.Compile();
+            return (instance, parameters) =>
+            {
+                execute(instance, parameters);
+                return null;
+            };
+        }
+        else
+        {
+            UnaryExpression castMethodCall = Expression.Convert(methodCall, typeof(object));
+            Expression<Func<object, object[], object>> lambda = Expression.Lambda<Func<object, object[], object>>(
+                castMethodCall, instanceParameter, parametersParameter);
+
+            return lambda.Compile();
+        }
     }
 
 }
